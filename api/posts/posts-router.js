a4ea902935273b4d00a35c1161b3256e01b97c00
unknown <Katherine2bk@gmail.com>
2021-04-12 14:28:39 -0700

    set up postsRouter, imported into server.js... everything is working so far

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,21 @@
-// implement your posts router here
+const express = require('express');
+const Posts = require('./posts-model');
+const router = express.Router();
+router.use(express.json());
+
+router.get('/', (req, res) => {
+    Posts.find()
+    .then(posts => {
+        res.json(posts);
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(500).json({
+            error: "The posts information could not be retrieved"
+        });
+    });
+});
+
+module.exports = router;
+
+
--- api/server.js
+++ api/server.js
@@ -2,7 +2,10 @@
 // require your posts router and connect it here
 const express = require('express');
 const router = express.Router();
+const postsRouter = require('./posts/posts-router');
 router.use(express.json());
+
+router.use('/posts', postsRouter);
 
 router.get('/', (req, res) => {
     res.send('Welcome to the API');
