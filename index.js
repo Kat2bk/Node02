935b7f4cfc6f1a5f68c865e7e36b5eab86dbe6b8
unknown <Katherine2bk@gmail.com>
2021-04-12 14:14:06 -0700

    install expressed, set up scripts in package.json, server is listening from index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// require your server and launch it here
+const express = require('express');
+
+const server = express();
+server.use(express.json());
+
+const port = 8080;
+
+server.get('/api', (req, res) => {
+    res.send('Welcome to the API');
+})
+
+server.listen(port, () => {
+    console.log(`server is running on localhost:${port}`);
+})
+
